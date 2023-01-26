3c94cd26b6ad3ed0069cf578b4f68bbe38b3c922
iremyildizz <iremyildiz02@gmail.com>
2023-01-25 23:29:38 -0500

    Ajout de fonction d'Intervalle

--- Labo 1/exercice 2/exercice 2.cpp
+++ Labo 1/exercice 2/exercice 2.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+bool dansIntervalle(float numero, float borneSuperieure = INFINITY, float borneInferieure = 0) {
+    return (numero > borneInferieure && numero < borneSuperieure);
+}
 
 int main()
 {
@@ -8,16 +13,17 @@ int main()
     float tauxAnnuel = 0.0;
     int nDeMois = 0;
     float tauxPayee = 0.0;
-    while (sommeDargent <= 0) {
+
+    while (!dansIntervalle(sommeDargent)) {
         cout << "La somme d'argent qui est plus grand que 0:" << endl;
         cin >> sommeDargent;
     }
-    while (montantRembourse <= 0) {
+    while (!dansIntervalle(montantRembourse)) {
         cout << "Le montant rembourse qui est plus grand que 0:" << endl;
         cin >> montantRembourse;
     }
-    while (tauxAnnuel <= 0 || tauxAnnuel >= 100) {
-        cout << "Le taux d'interet annuel qui est plus grand que 0:" << endl;
+    while (!dansIntervalle(tauxAnnuel, 100)) {
+        cout << "Le taux d'interet annuel qui est plus grand que 0 et plus petit que 100:" << endl;
         cin >> tauxAnnuel;
     }
     cout << "la somme d'argent:" << sommeDargent << endl << "Le montant rembourse:" << montantRembourse << endl << "Le taux d'interet annuel:" << tauxAnnuel << endl;
@@ -31,7 +37,7 @@ int main()
         sommeDargent -= montantRembourse;
         nDeMois += 1;
     }
-
+   
     cout << "Le nombre de mois necessaires: " << nDeMois << endl;
     cout << "Le taux total payee:" << tauxPayee << endl;
 
