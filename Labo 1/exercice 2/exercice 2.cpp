defbf7a08a1869797a77b5a914bba3be700a0c21
iremyildizz <iremyildiz02@gmail.com>
2023-01-25 22:52:31 -0500

    correction ex2

--- Labo 1/exercice 2/exercice 2.cpp
+++ Labo 1/exercice 2/exercice 2.cpp
@@ -3,26 +3,32 @@ using namespace std;
 
 int main()
 {
-    float sommeDargent;
-    float montantRembourse;
-    float tauxAnnuel;
+    float sommeDargent = 0.0;
+    float montantRembourse = 0.0;
+    float tauxAnnuel = 0.0;
     int nDeMois = 0;
-    float tauxPayee = 0;
-    cout << "La somme d'argent:" << endl;
-    cin >> sommeDargent;
-    cout << "Le montant rembourse:" << endl;
-    cin >> montantRembourse;
-    cout << "Le taux d'interet annuel:" << endl;
-    cin >> tauxAnnuel;
+    float tauxPayee = 0.0;
+    while (sommeDargent <= 0) {
+        cout << "La somme d'argent qui est plus grand que 0:" << endl;
+        cin >> sommeDargent;
+    }
+    while (montantRembourse <= 0) {
+        cout << "Le montant rembourse qui est plus grand que 0:" << endl;
+        cin >> montantRembourse;
+    }
+    while (tauxAnnuel <= 0 || tauxAnnuel >= 100) {
+        cout << "Le taux d'interet annuel qui est plus grand que 0:" << endl;
+        cin >> tauxAnnuel;
+    }
     cout << "la somme d'argent:" << sommeDargent << endl << "Le montant rembourse:" << montantRembourse << endl << "Le taux d'interet annuel:" << tauxAnnuel << endl;
 
     float tauxMensuel = tauxAnnuel / 12;
     cout << "Le taux d'interet mensuel:" << tauxMensuel << endl;
 
     while (sommeDargent > 0) {
-        sommeDargent -= montantRembourse;
         tauxPayee += sommeDargent * tauxMensuel / 100;
         sommeDargent += sommeDargent * tauxMensuel / 100;
+        sommeDargent -= montantRembourse;
         nDeMois += 1;
     }
 
