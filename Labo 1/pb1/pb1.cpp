5204ffb10c3d56e037cf77bd50ae931c1f002f28
iremyildizz <iremyildiz02@gmail.com>
2023-01-29 15:06:49 -0500

    exo 1 changements

--- Labo 1/pb1/pb1.cpp
+++ Labo 1/pb1/pb1.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 int main()
 {
-	while (true)
-	{
+	while (true) {
 		int nombre;
 		int diviseur = 2;
 		bool estPrime = true;
 
-		cout << "Entrez un nombre : ";
+		cout << "\nEntrez un nombre : ";
 		cin >> nombre;
 
 		if (nombre == 0 || nombre == 1 || (nombre != 2 && nombre % diviseur == 0))
@@ -17,30 +16,17 @@ int main()
 
 		if (estPrime)
 			for (diviseur = 3; diviseur <= sqrt(nombre); diviseur += 2)
-			{
-				if (nombre % diviseur == 0)
-				{
+				if (nombre % diviseur == 0) {
 					estPrime = false;
 					break;
 				}
-			}
 
 		if (estPrime)
-		{
-			cout << "Le nombre choisi est prime." << endl;
-		}
-
-		else
-		{
-
-			if (nombre == 0 || nombre == 1)
-			{
-				cout << "Le nombre choisi n'est pas prime " << endl;
-			}
-			else
-			{
-				cout << "Le nombre choisi n'est pas prime et son plus petit diviseur est " << diviseur << endl;
-			}
+			cout << "Le nombre choisi est prime.";
+		else {
+			cout << "Le nombre choisi n'est pas prime ";
+			if (nombre != 0 && nombre != 1)
+				cout << "et son plus petit diviseur est " << diviseur;				
 		}
 	}
 }
